97b95f4cf1ec5d8c68a1d825bdb441c884874369
Tran Hai Trieu <haitrieu2524@gmail.com>
2024-02-15 22:16:00 +0700

    Bổ sung audience field khi tạo một bài post

--- src/services/post.services.ts
+++ src/services/post.services.ts
@@ -13,12 +13,13 @@ import databaseService from './database.services'
 class PostService {
   // Thêm bài viết
   async createPost({ body, accountId }: { body: CreatePostReqBody; accountId: string }) {
-    const { images, content } = body
+    const { images, content, audience } = body
     const { insertedId } = await databaseService.posts.insertOne(
       new Post({
         accountId: new ObjectId(accountId),
         content,
-        images: images ? images.map((item) => new ObjectId(item)) : undefined
+        images: images ? images.map((item) => new ObjectId(item)) : undefined,
+        audience
       })
     )
     const newPost = await databaseService.posts.findOne({ _id: insertedId })
